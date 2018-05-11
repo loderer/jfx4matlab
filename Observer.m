c3a580d663d209023d759fbe9f3b30a9d2684f77
Rudi Loderer <loderer@hm.edu>
2018-05-11 11:12:21 +0200

    Improve documentation

--- Observer.m
+++ Observer.m
@@ -1,14 +1,16 @@
 classdef Observer
-    %CONTROLLERADAPTER Summary of this class goes here
-    %   Detailed explanation goes here
+    %OBSERVER An instance of this class observes the gui.
+    %   This class maps every event to an appropriate callback.
     
     properties
     end
     
     methods
-        function notify(obj, e) 
-            if(strcmp(e.fxId, 'PLOT_FUNCTIONS_CB') && strcmp(e.action, 'ON_CLICK'))
-                obj.togglePlotFunctions(controller);
+        function notify(~, e) 
+            if(strcmp(e.controller, 'package.class')...
+                    && strcmp(e.fxId, 'fxId')...
+                    && strcmp(e.action, 'action'))
+                % TODO
             else
                disp(['No callback registered.'...
                     ' (controller: ' char(e.controller)...
--- startup.m
+++ startup.m
@@ -1,13 +1,14 @@
 function startup()
-    % Benötigte Klassen zum statischen Klassenpfad hinzufügen. 
+    % Benötigte Klassen zum statischen Klassenpfad hinzufügen
    javaaddpathstatic(['C:\Program Files\MATLAB\R2015b\sys\java\jre\win64\jre\lib\jfxrt.jar']);
    javaaddpathstatic(['C:\Users\rudi\Documents\GitHub\BaJavaFx\out\production\JavaFxSample']);
    
+    % GUI starten
    jfxMain = javaObject('sample.Main');
    jfxMain.startGui('');
-      
-   observer = Observer; 
    
+   % Callbacks registrieren
+   observer = Observer; 
    observable_h = handle(jfxMain.getObservable(),'CallbackProperties');
    set(observable_h, 'UiEventCallback', @(h,e)observer.notify(e));
    
